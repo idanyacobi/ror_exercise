e422f265f97f956a6d190002b65173b1c94bf87d
Idan Yacobi <idan.yacobi@lemonade.com>
2021-01-06 15:58:40 +0200

    Yuval review fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: User.find(params[:id]).to_json
+    render json: User.find(params[:id])
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      render plain: user.id
+      render json: user
     else
-      render :status => 404
+      render :json => { :errors => user.errors.full_messages }, :status => 401
     end
   end
 
